f9834195afb2848ef7579d5ea0800e54e385a68f
Pablo Uraga Martinez <emptywhitelist@gmail.com>
2023-01-16 16:41:27 +0100

    6 wip

---
+++ src/Ejercicio06.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pabmar
+ */
+public class Ejercicio06 {
+//    Escribe un método, de nombre enviarMensaje, que reciba por
+//    parámetro una cadena de caracteres correspondiente a una dirección
+//    de correo electrónico. El método comprobará que la dirección recibida
+//    es correcta elevando la excepción DirCorreoIncorrectaExcepcion en
+//    caso contrario. La comprobación consistirá en verificar que la dirección
+//    contiene el carácter (@), algún carácter después de él antes del
+//    carácter (.) y algún carácter después de éste
+    public static void enviarMensaje(String cadena){
+        
+    }
+}
