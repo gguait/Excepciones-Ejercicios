56872991b42c30f6c7adba1fbd4cd61877d7c0cb
Pablo Uraga Martinez <emptywhitelist@gmail.com>
2023-01-18 20:04:34 +0100

    7 wip

---
+++ src/Ejercicio07.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pabmar
+ */
+public class Ejercicio07 {
+    public static void main(String[] args) {
+        Corredor primero = new Corredor(2);
+        primero.Correr();
+    }
+}
